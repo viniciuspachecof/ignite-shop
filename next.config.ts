0a1486079aa2793ae403e10010ca60674245592d
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-06-26 22:55:33 -0300

    Configurando Stripe e buscando os produtos

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  images: {
+    remotePatterns: [new URL('https://files.stripe.com/**')],
+  },
 };
 
 export default nextConfig;
